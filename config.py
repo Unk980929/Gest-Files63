3a5f3e4884cd8d60414fa8269af8d241369ff001
Kenchi17 <104829573+Kenchi17@users.noreply.github.com>
2022-07-02 21:17:14 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Ahora todo se configura desde el config.py
+
+TOKEN = '5182785203:AAHceuvNTMxX77VO6y_N2TrYAeh29L9b91A'
+API_ID = 13108769
+API_HASH = '080207b4f4d213b0ad99e15f08bfa982'
+ADMIN = 'Kenchi17'
+ROOT_DIR = 'root'
