41190867c301fde79687b2992619ad5b282ab582
Kenchi17 <104829573+Kenchi17@users.noreply.github.com>
2022-07-02 20:47:33 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 # Ahora todo se configura desde el config.py
 
-BOT_TOKEN = ''
-API_ID = 1
-API_HASH = ''
-ADMIN_USER = ''
+BOT_TOKEN = '5182785203:AAHpGXRExkDtuLDr8O0CadXafAapbyh7Txc'
+API_ID = 13108769
+API_HASH = '080207b4f4d213b0ad99e15f08bfa982'
+ADMIN_USER = 'Kenchi17'
 ROOT_DIR = 'root'
